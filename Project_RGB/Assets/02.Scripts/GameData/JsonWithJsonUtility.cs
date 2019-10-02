2340f7577ee78e9fb01755300ec9cbdcbf0a4236
DevDong95 <54832466+DevDong95@users.noreply.github.com>
2019-10-02 15:31:24 +0900

    Json and router basic
    1. Save a json file
    2. Open a json file
    3. Network router

---
+++ Project_RGB/Assets/02.Scripts/GameData/JsonWithJsonUtility.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class JsonWithJsonUtility : JsonConverter
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region JsonUtility 테스팅
+
+        //                                              1. ObjectToJson 테스트 ---> 1#
+        //JsonUtilityTestClass jtc = new JsonUtilityTestClass(true);
+        //string jsonData = ObjectToJsonWithJsonUtility(jtc);
+        //Debug.Log(jsonData);
+
+        //                                              2. JsonToOject 테스트 ---> 2#
+        //var jtc2 = JsonToOjectWithJsonUtility<JsonUtilityTestClass>(jsonData);
+        //jtc2.Print();
+
+        //                                              3. CreateJsonFile 테스트 ---> 1#
+        //JsonUtilityTestClass jtc = new JsonUtilityTestClass(true);
+        //string jsonData = ObjectToJson(jtc);
+        //CreateJsonFile(Application.dataPath, "JsonUtilityTestClass", jsonData);
+
+        //                                              4. CreateJsonFile 테스트 ---> 2#
+        //var jtc2 = LoadJsonFileWithJsonUtility<JsonUtilityTestClass>(Application.dataPath, "JTestClass");
+        //jtc2.Print();
+
+        //                                              JsonUtility로 MonoBehaviour를 상속받은 클래스의 오브젝트를 Serialize
+        //GameObject obj = new GameObject();
+        //obj.name = "TestMono 01";
+        //var jd = JsonUtility.ToJson(obj.GetComponent<TestMono>());
+        //Debug.Log(jd);
+
+        //                                              MonoBehaviour를 상속받은 클래스의 오브젝트를 Deserialize 할 때,
+        //                                              FromJson 메서드 대신 FromJsonOverwrite 메서드 사용.
+        //                                              이 함수는 JSON 데이터를 오브젝트로 변환할 때, 새로운 오브젝트를 만들지 않고 기존에 있는 오브젝트에 클래스의 변수 값을 덮어씌우는 처리를 한다.
+        //GameObject obj = new GameObject();
+        //obj.name = "TestMono 01";
+        //var t = obj.AddComponent<TestMono>();
+        //t.i = 333;
+        //t.pos = new Vector3(-939, -33, -22);
+        //var jd = JsonUtility.ToJson(obj.GetComponent<TestMono>());
+        //Debug.Log(jd);
+
+        //GameObject obj2 = new GameObject();
+        //obj2.name = "TestMono 02";
+        //var t2 = obj2.AddComponent<TestMono>();
+        //JsonUtility.FromJsonOverwrite(jd, t2);
+        #endregion
+    }
+
+    /* Object ---> JSON 문자열 */
+    public string ObjectToJsonWithJsonUtility(object obj)
+    {
+        return JsonUtility.ToJson(obj);
+    }
+
+    /* 문자열 JSON ---> 템플릿 객체 */
+    public T JsonToOjectWithJsonUtility<T>(string jsonData)
+    {
+        return JsonUtility.FromJson<T>(jsonData);
+    }
+}
---
+++ Project_RGB/Assets/02.Scripts/GameData/JsonWithNewtonsoft.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;           // JSON의 직렬화를 사용하기 위한 dll 파일
+
+
+public class JsonWithNewtonsoft : JsonConverter
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region Newtonsoft 테스팅
+
+        //                                                  1. 시리얼라이즈 테스트
+        //NewtonsoftTestClass jtc = new NewtonsoftTestClass(true); 
+        //string jsonData = ObjectToJsonWithNewtonsoft(jtc);
+        //Debug.Log(jsonData);
+
+        //                                                  2. 디시리얼라이즈 테스트
+        //var jtc2 = JsonToOjectWithNewtonsoft<NewtonsoftTestClass>(jsonData);
+        //jtc2.Print();
+
+        //                                                  3. 파일화테스트
+        //NewtonsoftTestClass jtc = new NewtonsoftTestClass(true);
+        //string jsonData = ObjectToJsonWithNewtonsoft(jtc);
+        //CreateJsonFile(Application.dataPath, "JTestClass", jsonData);
+
+        //                                                  4. 파일 로드
+        //var jtc2 = LoadJsonFile<NewtonsoftTestClass>(Application.dataPath, "JTestClass");
+        //jtc2.Print();
+
+        #endregion
+    }
+
+    /* 오브젝트를 문자열로 된 JSON 데이터로 변환처리 */
+    public string ObjectToJsonWithNewtonsoft(object obj)
+    {
+        return JsonConvert.SerializeObject(obj);
+    }
+
+    /* 문자열로 된 JSON 데이터를 받아서 원하는 타입의 객체(템플릿)으로 반환처리 */
+    public T JsonToOjectWithNewtonsoft<T>(string jsonData)
+    {
+        return JsonConvert.DeserializeObject<T>(jsonData);
+    }
+}
