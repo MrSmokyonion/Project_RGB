bbda09b54cb459eba0db6c723c28b5d29df5170c
KimRyuan <kdh990705@gmail.com>
2019-12-03 08:54:24 +0900

    1203_AM0900
    Commit

--- Project_RGB/Assets/02.Scripts/UI/Dungeon/DungeonUI/NoticeTextUI.cs
+++ Project_RGB/Assets/02.Scripts/UI/Dungeon/DungeonUI/NoticeTextUI.cs
@@ -25,7 +25,9 @@ public class NoticeTextUI : MonoBehaviour
         for (float a = 255; a >= 0; a -= 10)
         {
             transform.GetComponent<Text>().color = new Color(255 / 255f, 255 / 255f, 255 / 255f, a / 255f);
+            transform.parent.GetComponent<Text>().color = new Color(255 / 255f, 255 / 255f, 255 / 255f, a / 255f);
             transform.GetChild(0).GetComponent<Image>().color = new Color(255 / 255f, 255 / 255f, 255 / 255f, a / 255f);
+            transform.GetChild(0).GetChild(0).GetComponent<Image>().color = new Color(255 / 255f, 255 / 255f, 255 / 255f, a / 255f);
             yield return new WaitForFixedUpdate();
         }
 
@@ -39,9 +41,13 @@ public class NoticeTextUI : MonoBehaviour
         for (float a = 0; a >= 0; a += 10)
         {
             transform.GetComponent<Text>().enabled = true;
+            transform.parent.GetComponent<Image>().enabled = true;
             transform.GetChild(0).GetComponent<Image>().enabled = true;
+            transform.GetChild(0).GetChild(0).GetComponent<Image>().enabled = true;
             transform.GetComponent<Text>().color = new Color(255 / 255f, 255 / 255f, 255 / 255f, a / 255f);
+            transform.parent.GetComponent<Text>().color = new Color(255 / 255f, 255 / 255f, 255 / 255f, a / 255f);
             transform.GetChild(0).GetComponent<Image>().color = new Color(255 / 255f, 255 / 255f, 255 / 255f, a / 255f);
+            transform.GetChild(0).GetChild(0).GetComponent<Image>().color = new Color(255 / 255f, 255 / 255f, 255 / 255f, a / 255f);
             yield return new WaitForFixedUpdate();
         }
 
